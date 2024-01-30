191823dc5cae4e1acb3e9c6c0c691c7ee8cb428b
Anton <anton333mai@gmail.com>
2024-01-30 17:46:49 +0900

    fixed saved page and link to the posts

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -13,10 +13,28 @@ const Saved = () => {
   });
 
   return (
-    <div className="flex justify-center flex-wrap gap-9 w-full max-w-5xl">
-      {savedPosts.length !== 0 && (
-        <GridPostList key={savedPosts.id} posts={savedPosts} />
-      )}
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <div className="flex-between w-full max-w-5xl mt-16 mb-7">
+          <h3 className="body-bold md:h3-bold">Saved Posts</h3>
+
+          <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+            <p className="small-medium md:base-medium text-light-2">All</p>
+            <img
+              src="/assets/icons/filter.svg"
+              width={20}
+              height={20}
+              alt="filter"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center flex-wrap gap-9 w-full max-w-5xl">
+          {savedPosts.length !== 0 && (
+            <GridPostList key={savedPosts.id} posts={savedPosts} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
